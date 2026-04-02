e05274d88a44c0fe1584663b51824dd0ca1866de
GiorgiTch204 <gtcheishvili204@gmail.com>
2026-04-02 12:38:08 +0400

    added toggle for dark mode and light mode switcher

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 import Link from "next/link";
 
 export default function Header(){
+    const [darkMode, setDarkMode]=useState(false);
+
+    useEffect(() => {
+        const root=document.documentElement;
+
+        if(darkMode){
+            root.classList.add("dark");
+        }else{
+            root.classList.remove("dark");
+        }
+    }, [darkMode]);
+
     return(
         <>
             <header className="headerContainer">
                 <h1>My Website</h1>
 
-                <div>
+                <div style={{
+                    display:"flex",
+                    gap:"10px",
+                    alignItems:"center"
+                }}>
+
+                    <button 
+                        onClick={() => setDarkMode(!darkMode)}
+                        style={{cursor:"pointer"}}
+                    >
+                        {darkMode ? "☀️" : "🌙"}
+                    </button>
+
                     <Link href="/register">Register</Link>
                     <Link href="/login">Login</Link>
                 </div>
